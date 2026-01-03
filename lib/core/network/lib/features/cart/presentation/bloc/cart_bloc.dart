a82d21633c63e7558cbdc487693cb89272ba28df
Dilmurod1985 <87082624+Dilmurod1985@users.noreply.github.com>
2026-01-03 22:26:52 +0500

    Implement CartBloc with events and states

---
+++ lib/core/network/lib/features/cart/presentation/bloc/cart_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class CartEvent {}
+class LoadCart extends CartEvent {}
+
+abstract class CartState {}
+class CartLoading extends CartState {}
+class CartLoaded extends CartState {
+  final List<String> items;
+  final double totalPrice;
+  CartLoaded(this.items, this.totalPrice);
+}
+
+class CartBloc extends Bloc<CartEvent, CartState> {
+  CartBloc() : super(CartLoading()) {
+    on<LoadCart>((event, emit) async {
+      // Имитация загрузки данных
+      await Future.delayed(const Duration(seconds: 1));
+      emit(CartLoaded(['Товар 1', 'Товар 2'], 1500.0));
+    });
+  }
+}
